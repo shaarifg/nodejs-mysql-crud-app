556c9724a7d998791b3abf75552989af9569e202
Mohammad Sharif <ermohdsharif@gmail.com>
2023-03-13 19:01:56 +0530

    added bookRouter

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 require("./db");
 const userRouter = require("./routes/user.routes");
+const bookRouter = require("./routes/book.routes");
 const { userModel, bookModel } = require("./models");
 
 const app = express();
 app.use(express.json());
 app.use("/", userRouter);
+app.use("/", bookRouter);
 
 userModel.sync();
 bookModel.sync();
