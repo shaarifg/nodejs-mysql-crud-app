c16e2a1209486f32aa05424a20cffc810fb82be7
Mohammad Sharif <ermohdsharif@gmail.com>
2023-03-13 18:43:12 +0530

    added book fields

--- models/book.js
+++ models/book.js
@@ -1,23 +1,25 @@
-const { DataTypes, Model } = require("sequelize");
 const sequelize = require("../db");
+const { DataTypes, Model } = require("sequelize");
+
 class Book extends Model {}
 
 Book.init(
   {
     // Model attributes are defined here
-    bookName: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    author: DataTypes.STRING,
     price: {
-      type: DataTypes.NUMBER,
-      defaultValue: 1,
+      type: DataTypes.STRING,
     },
   },
   {
     // Other model options go here
     sequelize, // We need to pass the connection instance
     modelName: "Book", // We need to choose the model name
+    timestamps: false,
   }
 );
 
