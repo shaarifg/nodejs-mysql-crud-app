97f0d3d43351dd27fde55fb5a7636e2ff6dcd10b
Mohammad Sharif <ermohdsharif@gmail.com>
2023-03-14 10:40:59 +0530

    added update and delete routes

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,7 +1,15 @@
-const { addBook } = require("../controllers/book.controller");
+const {
+  addBook,
+  getAllBooks,
+  updateBook,
+  deleteBook,
+} = require("../controllers/book.controller");
 
 const router = require("express").Router();
 
 router.post("/books", addBook);
+router.get("/books", getAllBooks);
+router.put("/books", updateBook);
+router.delete("/books", deleteBook);
 
 module.exports = router;
