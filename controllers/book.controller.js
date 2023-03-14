73a1d7dd24d32d948adf8178dbd4a507f4e7167a
Mohammad Sharif <ermohdsharif@gmail.com>
2023-03-14 10:40:23 +0530

    added update and delete methods

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -17,6 +17,54 @@ const addBook = async (req, res) => {
   }
 };
 
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await bookModel.findAll();
+    return res.status(200).json({ message: "found books", data: books });
+  } catch (error) {
+    return res.status(500).json({ message: "server error", error: error });
+  }
+};
+
+// Function to update a book
+const updateBook = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await userModel.update(
+      {
+        price: req.body.price,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    console.log(user);
+    res.status(202).json("book updated");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Function to delete the book
+const deleteBook = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await userModel.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).json({ message: "book deleted" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 module.exports = {
   addBook,
+  getAllBooks,
+  updateBook,
+  deleteBook,
 };
